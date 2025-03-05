898d86602899905a6993f0f111b68dfaaaa71274
bbydolly <cristinalopezdelcastilloinf1@gmail.com>
2025-03-05 20:34:27 +0100

    Agregar archivos de proyecto.

---
+++ UserControl1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace ValidateTextBox
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInterno = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtInterno
+            // 
+            this.txtInterno.Location = new System.Drawing.Point(10, 10);
+            this.txtInterno.Name = "txtInterno";
+            this.txtInterno.Size = new System.Drawing.Size(100, 20);
+            this.txtInterno.TabIndex = 0;
+            this.txtInterno.TextChanged += new System.EventHandler(this.txtInterno_TextChanged);
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtInterno);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(123, 42);
+         
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInterno;
+    }
+}
---
+++ UserControl1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.Remoting.Messaging;
+
+namespace ValidateTextBox
+{
+
+    //Declaración del enumerado 
+    public enum eTipo
+    {
+        NUMERICO, TEXTUAL
+    }
+
+
+
+    public partial class UserControl1 : UserControl
+    {
+        public int index = 0;
+        public Color color = Color.Red;
+        public bool auxTextual = false;
+
+        char[] letras = new char[53];
+        public UserControl1()
+        {
+            InitializeComponent();
+            //  this.Size = new Size(txtInterno.Height + 20, );
+            this.Height = txtInterno.Height + 20;
+            txtInterno.Width = this.Width - 20;
+
+
+            //Bucle de iniciación de las letras (mayusculas, minusculas y un espacio)--53 caracteres
+            for (int i = 0; i < 26; i++)// 26 porque hago una vuelta del abecedario
+            {
+
+                letras[i] = (char)('A' + i);
+                //Darle a depurar para visualizar las letars 
+                Debug.Write(letras[i]);
+                letras[i + 26] = (char)('a' + i);
+            }
+            letras[52] = ' ';
+
+
+        }
+
+        //Definicion de nuevas propiedades de mi nuevo componente
+
+        private String texto;
+        [Category("Appearence")]
+        [Description("Accede al campo text del TextBox interno del componente")]
+
+        public String Texto
+        {
+            get
+            {
+                return txtInterno.Text;
+
+            }
+            set
+            {
+
+                if (Tipo == eTipo.NUMERICO)
+                {
+                    String aux = value;
+
+
+                    if (aux != "")
+                    {
+
+
+
+                        bool flag = comprobarNumIndex(aux, 0);
+
+
+
+                        Debug.WriteLine(flag + " bandera");
+                        if (flag) { color = Color.Green; }
+                        if (!flag) { color = Color.Red; /*isRecursivo = true;*/ }
+                    }
+
+
+
+                }
+                else if (Tipo == eTipo.TEXTUAL)
+                {
+                    String auxtext = value;
+
+                    auxTextual = comprobarTextual(auxtext, 0);
+
+                    if (auxTextual) { color = Color.Green; }
+                    if (!auxTextual) { color = Color.Red; }
+
+
+                }
+                /* txtInterno.Text = value; //se *//*refiere a esto? */
+                txtInterno.Text = value;
+                Invalidate();
+                //Refresh();
+
+
+            }
+        }
+
+
+        //Propiedad Multiline
+        private bool multilinea;
+
+        [Category("Appearence")]
+        [Description("Establece la propiedad multiline del text box interno del componente")]
+        public bool Multilinea
+        {
+            get { return txtInterno.Multiline; }
+            set
+            {
+                txtInterno.Multiline = value;
+
+
+            }
+        }
+
+        //Propiedad de tipo eTipo
+        private eTipo tipo;
+        [Category("Appearence")]
+        [Description("Determina si es de tipo NUMÉRICO o TEXTUAL")]
+
+        public eTipo Tipo
+        {
+            get
+            {
+                return tipo;
+            }
+            set
+            {
+
+                tipo = value;
+            }
+
+
+        }
+
+
+        //Definición del evento y forma de agregarlo 
+        [Category("Mis eventos")]
+        [Description("Permite el acceso TextChange del textBox interno del componente.")]
+        public event EventHandler CambiaTexto;
+
+        //Función asociada a mi evento
+
+        public void OnCambiaTexto()
+        {
+            //Si mi evento no es nulo se invoca
+            CambiaTexto?.Invoke(this, EventArgs.Empty);
+        }
+
+
+
+
+        // HERENCIA Y SOBRECARGA de UserControl que a suvez hereda de Control que tiene el método
+        // SizeChange 
+
+        // Al sobreescribir el método se usa OnXX ,SIENDO XX el evento a sobreescribir de la clase padre
+
+        // Método sobreescrito de la clase UserControl>Control por lo que no necesita enlazarlo desde las propiedades
+        // de mi componente
+
+        //Si existe el método se recomienda sobreescribirlo
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            this.Height = txtInterno.Height + 20;
+            txtInterno.Width = this.Width - 20;
+            Debug.WriteLine("aaaaaaaaaaaaaaaaaaa");
+        }
+
+        private void txtInterno_TextChanged(object sender, EventArgs e)
+        {
+            Texto = txtInterno.Text;
+            Debug.WriteLine("Hago el set del texto a mi componente");
+        }
+
+
+
+
+
+        public bool comprobarNumIndex(String valor, int index)
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+
+
+
+
+            if (valor.Length - 1 >= index)
+            {
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    try
+                    {
+                        int valoraux = Int32.Parse(valor[index].ToString());
+                        if (valoraux == (nums[i]))
+                        {
+                            Debug.WriteLine(valor[index] + " valor index");
+
+
+                            if (valor.Length - 1 == index)
+                            {
+                                //color = Color.Green;
+                                return true;
+                            }
+                            else
+                            {
+                                //Devuelvo el resultado de la funcion que es true o false
+                                return comprobarNumIndex(valor, index + 1);//compruebo el siguiente numero
+
+                            }
+                        }
+                    }
+                    catch (FormatException fw)
+                    {
+
+                        return false;
+                    }
+                    catch (ArgumentException arg)
+                    {
+
+                        return false;
+                    }
+                    catch (OverflowException overflow)
+                    {
+                        return false;
+                    }
+
+
+
+                }
+
+            }
+
+
+
+            return false;
+
+        }
+
+        public bool comprobarTextual(String cad, int index)//index 0
+        {
+            if (cad != "")
+            {
+
+                int longitudCad = cad.Length;
+
+                foreach (var item in cad)
+                {
+                    auxTextual = letras.Contains(item);//si no lo contiene
+
+                    if (!auxTextual)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            // Graphics g = e.Graphics;
+            // Color color = Tipo == eTipo.NUMERICO ? Tipo == eTipo.TEXTUAL ? Color.Green : Color.Red: Color.Red;
+            e.Graphics.DrawRectangle(new Pen(color, 2), new Rectangle(5, 5, this.Width - 5, this.Height - 5));
+            // Refresh();
+        }
+
+    }
+}
